2f4cb93fe0f5f73fffad01c3e06f1925a3622722
Jain <shashank.jain01@sap.com>
2023-07-25 14:44:43 +0530

    added ui

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.post('/upload', upload.single('audioFile'), (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${port}`);
 });
